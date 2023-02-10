e3059b1cfb0488efa8822ee21b564ffdc8a59dfd
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-02-10 14:53:53 +0530

    Initial Commit -- App setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 1338;
+
+app.use(express.json());
+app.use(express.static('content'));
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+    console.log(`App is running on PORT : ${PORT}`);
+});
